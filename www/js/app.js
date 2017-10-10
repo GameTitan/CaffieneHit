a9f2c595bac9e7cf3f61d6b3f652efc8860092c5
Sidd Sara <saraf.siddharth@gmail.com>
2017-10-09 20:49:57 -0700

    initial files

---
+++ www/js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('caffeinehit', [
+	'ionic',
+	'ngMap',
+	'ngCordova',
+	'caffeinehit.controllers',
+	'caffeinehit.services',
+	'caffeinehit.filters'
+]);
+
+app.run(function ($ionicPlatform) {
+	$ionicPlatform.ready(function () {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
+
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
+});
+
+app.config(function ($httpProvider) {
+	$httpProvider.defaults.headers.common['Authorization'] = 'Token fbfd86e6d6531b960421518ac2c36fc64d25e14d';
+});
